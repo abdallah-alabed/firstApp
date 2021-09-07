8fd1eff4d6352e69bf18085f62fe42ea90d8ffe6
abdallah-alabed <abd.alabed97@gmail.com>
2021-09-07 13:49:35 +0300

    fix the compile

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Main from './components/Main'
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Header/>
+        <Header />
         <Main />
-        <Footer/>
+        <Footer />
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
